85cb076e2abc074724e5ebe0f0b51ba9a0b25d27
Toru <t02f447f@gmail.com>
2022-05-06 06:06:39 +0900

    webサーバーの立ち上げ

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 type Page struct {
@@ -24,11 +26,15 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// URLは/view/test
+	// 「/view/」の６文字分削除したURLを取得
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-	p1 := &Page{Title: "test", Body: []byte("This is a sample page.")}
-	p1.save()
-
-	p2, _ := loadPage(p1.Title)
-	fmt.Println(string(p2.Body))
-
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
