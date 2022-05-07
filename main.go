a8e890e12283386c86cb1bf7b7ab2edf225cabc1
Toru <t02f447f@gmail.com>
2022-05-07 15:25:59 +0900

    テンプレートファイルの使い方を学習

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -26,15 +26,34 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	// template.ParseFilesでテンプレートオブジェクトを作成する
+	t, _ := template.ParseFiles(tmpl + ".html")
+	// TemplateオブジェクトのExecuteメソッドで値を埋め込む
+	t.Execute(w, p) // 第１引数：出力先、第２引数：埋め込むデータ
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// URLは/view/test
 	// 「/view/」の６文字分削除したURLを取得
 	title := r.URL.Path[len("/view/"):]
+	// title（ファイル名）を指定し、textファイルの中身をpとする
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	// URLは/edit/test
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
